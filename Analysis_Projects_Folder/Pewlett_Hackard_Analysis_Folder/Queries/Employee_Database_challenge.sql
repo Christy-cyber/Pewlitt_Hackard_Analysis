7a19952c6c64f70c0473971cfe340c6052ad13c6
unknown <drctcarter@gmail.com>
2022-01-31 15:41:13 -0500

    Adding Employee_Database_challenge file

--- Analysis_Projects_Folder/Pewlett_Hackard_Analysis_Folder/Queries/Employee_Database_challenge.sql
+++ Analysis_Projects_Folder/Pewlett_Hackard_Analysis_Folder/Queries/Employee_Database_challenge.sql
@@ -1,1 +1,37 @@
+-- Challenge Deliverable 1--Steps 1-7
+-- Create new Table -retirement titles- from employee and
+-- titles tables and filter on birthdate
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	ti.title,
+	ti.from_date, 
+	ti.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ti
+ON e.emp_no = ti.emp_no 
+WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY e.emp_no ASC;
 
+
+-- Challenge Deliverable 1--Steps 8-15--create a -unique_titles-
+-- table from the -retirement_titles- table using DISTINCT
+-- ON emp_no and filter by to_date
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = ('9999-01-01')
+ORDER BY emp_no ASC, to_date DESC;
+
+-- Challenge Deliverable 1--Steps 16-22
+-- Retrieve no of retiring employees by their most recent
+-- job title 
+SELECT COUNT(emp_no), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
